8b7401c8924e4c18312a843ed4da8ca12818ea01
Anton Pakhorukov <pakhorukov.a@mail.ru>
2022-09-20 00:00:10 +0700

    Finish

---
+++ Example_003/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int n = array.Length;
+int index = 0;
+int find = 510;
+
+while (index < n) {
+    if (array[index] == find) {
+        Console.WriteLine(index);
+        break; // ставим новый оператор, чтобы он при=ерывал команду, как только найдет число, 
+        //а не выводил все искомые числа, когда их несколько в массиве
+    } 
+    index++;
+}
